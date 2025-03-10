9c5843b07259cdee35d3ced4977ece0e7edaf34f
tharsis-soares <tharsissoares@hotmail.com>
2025-03-10 10:34:39 -0300

    day_1-2

---
+++ day-1/exercises.py
@@ -0,0 +1,18 @@
+print('1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.')
+print('2. Knead the dough for 10 minutes.')
+print('3. Add 3g of Salt.')
+print('4. Leave to rise for 2 hours.')
+print('5. Bake at 200 degrees C for 30 minutes.')
+
+#fix de code below
+#print(Notes from Day 1")
+# print("The print statement is used to output strings")
+#print("Strings are strings of characters"
+#priint("String Concatenation is done with the + sign")
+#print(("New lines can be created with a \ and the letter n")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the" + "sign")
+print("New lines can be created with a \ and the letter n")
